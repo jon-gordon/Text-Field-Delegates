6530d5faed0cd53e51e6e5796fa8cd3e467eff81
Jon Gordon <jon@jongor-software.com>
2015-05-17 21:45:52 +0100

    Initial Commit

---
+++ Text Field Delegates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text Field Delegates
+//
+//  Created by Jon Gordon on 17/05/2015.
+//  Copyright (c) 2015 JonGor Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Text Field DelegatesTests/Text_Field_DelegatesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Text_Field_DelegatesTests.swift
+//  Text Field DelegatesTests
+//
+//  Created by Jon Gordon on 17/05/2015.
+//  Copyright (c) 2015 JonGor Software. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Text_Field_DelegatesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
